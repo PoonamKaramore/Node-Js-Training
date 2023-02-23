8e9a8ab74712af7712fa885f71a995258077c716
Pravin Karamore <poonam.pravin-karamore@capgemini.com>
2023-02-23 20:23:19 +0530

    adding file

--- es6/arrowfunction.js
+++ es6/arrowfunction.js
@@ -1,11 +1,11 @@
 let operations = (a, b) => {
 
-    let sum = a + b;
+    sum = a + b;
     console.log('value of a and b is : '+ sum)
     
-    let substract = a-b;
+    substract = a-b;
     console.log('Value of a and b is : '+ substract)
-    
+
     let multiply = a*b;
     console.log('value of a and b is : '+ multiply)
 }
